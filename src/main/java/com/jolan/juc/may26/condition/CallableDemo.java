622c8ab2a2c93e13c949ddb64f258e7a32f30c08
wanfql5 <wangql5@yusys.com.cn>
2020-05-27 21:16:38 +0800

    juc study  callable-2 2020-05-27 21:16

--- src/main/java/com/jolan/juc/may26/condition/CallableDemo.java
+++ src/main/java/com/jolan/juc/may26/condition/CallableDemo.java
@@ -1,6 +1,9 @@
 package com.jolan.juc.may26.condition;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author FC Bayern Munich
@@ -9,8 +12,22 @@ import java.util.concurrent.Callable;
  *
  */
 public class CallableDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+//        Thread t1 = new Thread(new MyThread(), "A");//原始方式
 
+        //未加泛型
+//        FutureTask futureTask = new FutureTask(new MyThread2());
+//        new Thread(futureTask, "A").start();
+//        Integer result = (Integer)futureTask.get();
+//        System.out.println(result);
+
+        FutureTask<Integer> futureTask2 = new FutureTask<Integer>(new MyThread2());//加泛型
+        new Thread(futureTask2, "B").start();
+
+        System.out.println("*******主线程结束");
+
+        Integer result2 = futureTask2.get();
+        System.out.println(result2);
     }
 }
 
@@ -24,6 +41,12 @@ class MyThread implements Runnable{
 class MyThread2 implements Callable<Integer> {
     @Override
     public Integer call() throws Exception {
+        System.out.println("**********come in call");
+        try{
+            TimeUnit.SECONDS.sleep(4);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
         return 1024;
     }
 }
