f604f10236ff8f3e050dcb13e42ef9a4088c9cd0
wanfql5 <wangql5@yusys.com.cn>
2020-06-17 09:21:36 +0800

    jvm study  heap 20200617 09:21

--- src/main/java/com/jolan/jvm/jun13/JVMNote.java
+++ src/main/java/com/jolan/jvm/jun13/JVMNote.java
@@ -26,6 +26,19 @@ package com.jolan.jvm.jun13;
  * 7 堆
  *  7.1 一个JVM实例只存在一个堆内存，堆内存的大小是可以调解的。类加载器读取了类文件后，需要把类、方法、常变量放到堆内存中，保存所有引用类型的真是信息，以方便执行器执行。
  *      堆内存逻辑上分为三部分：新生+养老+永久（元空间）
+ *  7.2
+ *      Eden满了，开启GC = YGC = 轻GC ，YGC会把Eden区域基本清空
+ *          S0(Survivor pace) = from
+ *          S1(Survivor pace) = to
+ *          from区和to区，他们的位置不固定，每次GC后会交换，GC之后有交换，谁空谁是to
+ *      Old养老区满了，开区Full GC = FGC。FGC多次发现养老区无法腾出更多空间，报错Out Of Memory OOM
+ *      Eden:from:to = 8:1:1，from和to永远相等。Young:Old = 1:2
+ *      MinorGC过程：复制->清空->互换。
+ *          1.首先，当Eden区满的时候会触发第一次GC，把还活着的对象拷贝到SurvivorFrom区，当Eden区再次触发GC的时候会扫描Eden区和from区，对这两个区域进行垃圾回收，
+ *              经过这次回收后还存货的对象，则直接复制到To区（如果有对象的年龄已经达到了老年的标准，则复制到老年代区），同时把这些对象的年龄+1
+ *          2.清空Eden区和SurvivorFrom中的对象。
+ *          3.SurvivorTo和SurvivorFrom交换：最后SurvivorTo和SurvivorFrom互换，原SurvivorTo成为下一次GC时的SurvivorFrom区。部分对象会在From和To区域中复制来复制去，如此交换15次（有JVM参数MaxTenuringThreshold
+ *              决定，这个参数默认是15），最终如果还是存货，就存入老年代。
  *
  *
  *  */
